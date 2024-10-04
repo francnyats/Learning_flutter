3b2d20c47a6942896782e9e37918cea5aad80e18
francnyats <francisnyatundo@gmail.com>
2024-10-04 11:22:02 +0300

    Showin expense, overlay for user input. add and remove expense

---
+++ beyond_basic/lib/widgets/expenses.dart
@@ -0,0 +1,81 @@
+import 'package:beyond_basic/widgets/expenses_list/expenses_list.dart';
+import 'package:beyond_basic/models/expense.dart';
+import 'package:beyond_basic/widgets/new_expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+//dummy data
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    )
+  ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      isScrollControlled:
+          true, //makes overlay take the whole screen, avoid keyboard obscuring buttons
+      context: context,
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
+    );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  void _remmoveExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Flutter Expense-Tracker',
+          style: TextStyle(fontSize: 20),
+        ),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('The Chart'),
+          Expanded(
+            child: ExpenseList(
+              expenses: _registeredExpenses,
+              onRemoveExpense: _remmoveExpense,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
