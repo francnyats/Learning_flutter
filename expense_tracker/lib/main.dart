adf9bf463b6f0253dde38deecc400ffd3fa9e9c4
francnyats <francisnyatundo@gmail.com>
2024-12-05 17:02:18 +0300

    Theming for card and button

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -11,11 +11,21 @@ void main() {
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
-          colorScheme: kColorScheme,
-          appBarTheme: const AppBarTheme().copyWith(
-            backgroundColor: kColorScheme.onPrimaryContainer,
-            foregroundColor: kColorScheme.primaryContainer,
-          )),
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+      ),
       home: const Expenses(),
     ),
   );
